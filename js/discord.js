9a09f8fd80e1869ff12370df6cb50325dcff2f27
unknown <mertushkaonline@gmail.com>
2022-09-03 23:06:17 +0300

    goofy ahh: speed improvements 💀

--- js/discord.js
+++ js/discord.js
@@ -1,67 +1,72 @@
-const OPCODES = {
-  INFO: 0,
-  HELLO: 1,
-  INIT: 2,
-  HEARTBEAT: 3,
-};
+window.onload = function () {
+  const OPCODES = {
+    INFO: 0,
+    HELLO: 1,
+    INIT: 2,
+    HEARTBEAT: 3,
+  };
 
-const elements = {
-  username: document.getElementById("username"),
-  avatar: document.getElementById("avatar"),
-  card: document.getElementById("profile"),
-  presence: document.getElementById("discord-presence"),
-};
+  const elements = {
+    username: document.getElementById("username"),
+    avatar: document.getElementById("avatar"),
+    card: document.getElementById("profile"),
+    presence: document.getElementById("discord-presence"),
+  };
 
-const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
+  elements.presence.src =
+    "https://lanyard-visualizer.netlify.app/user/940131816692674591?background=false&title=false&mode=iframe";
 
-// On Message
-lanyard.onmessage = ({ data }) => {
-  const parsedData = JSON.parse(data);
-  if (parsedData.d?.activities?.find((a) => a.type === 0) == undefined) {
-    elements.presence.setAttribute("hidden", "hidden");
-  } else {
-    elements.presence.removeAttribute("hidden");
-  }
+  const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
 
-  if (parsedData.op == OPCODES.HELLO) {
-    // Identify
-    lanyard.send(
-      JSON.stringify({
-        op: OPCODES.INIT,
-        d: {
-          subscribe_to_id: "940131816692674591",
-        },
-      })
-    );
+  // On Message
+  lanyard.onmessage = ({ data }) => {
+    const parsedData = JSON.parse(data);
+    if (parsedData.d?.activities?.find((a) => a.type === 0) == undefined) {
+      elements.presence.setAttribute("hidden", "hidden");
+    } else {
+      elements.presence.removeAttribute("hidden");
+    }
 
-    // Interval
-    setInterval(function () {
+    if (parsedData.op == OPCODES.HELLO) {
+      // Identify
       lanyard.send(
         JSON.stringify({
-          op: OPCODES.HEARTBEAT,
+          op: OPCODES.INIT,
+          d: {
+            subscribe_to_id: "940131816692674591",
+          },
         })
       );
-    }, parsedData.d.heartbeat_interval);
-  } else if (parsedData.op == OPCODES.INFO) {
-    const statusColors = {
-      online: "green",
-      offline: "gray",
-      idle: "yellow",
-      dnd: "pink",
-    };
 
-    if (parsedData.t == "INIT_STATE") {
-      const user = parsedData.d;
+      // Interval
+      setInterval(function () {
+        lanyard.send(
+          JSON.stringify({
+            op: OPCODES.HEARTBEAT,
+          })
+        );
+      }, parsedData.d.heartbeat_interval);
+    } else if (parsedData.op == OPCODES.INFO) {
+      const statusColors = {
+        online: "green",
+        offline: "gray",
+        idle: "yellow",
+        dnd: "pink",
+      };
 
-      elements.card.style.opacity = "1";
-      elements.username.innerText =
-        user.discord_user.username + `#${user.discord_user.discriminator}`;
+      if (parsedData.t == "INIT_STATE") {
+        const user = parsedData.d;
 
-      elements.avatar.src = `https://cdn.discordapp.com/avatars/${user.discord_user.id}/${user.discord_user.avatar}`;
-      setActiveStyle(statusColors[user.discord_status]);
-    } else if (parsedData.t == "PRESENCE_UPDATE") {
-      const user = parsedData.d;
-      setActiveStyle(statusColors[user.discord_status]);
+        elements.card.style.opacity = "1";
+        elements.username.innerText =
+          user.discord_user.username + `#${user.discord_user.discriminator}`;
+
+        elements.avatar.src = `https://cdn.discordapp.com/avatars/${user.discord_user.id}/${user.discord_user.avatar}`;
+        setActiveStyle(statusColors[user.discord_status]);
+      } else if (parsedData.t == "PRESENCE_UPDATE") {
+        const user = parsedData.d;
+        setActiveStyle(statusColors[user.discord_status]);
+      }
     }
-  }
+  };
 };
--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,12 +1,39 @@
-const links = document.querySelectorAll(".alternate-style"),
-  totalLinks = links.length;
-
 function setActiveStyle(color) {
-  for (let i = 0; i < totalLinks; i++) {
-    if (color === links[i].getAttribute("title")) {
-      links[i].removeAttribute("disabled");
-    } else {
-      links[i].setAttribute("disabled", true);
+  let links = document.querySelectorAll(".alternate-style"),
+    totalLinks = links.length;
+
+  if (
+    links.length === 0 ||
+    !Array.from(links).find((link) => link.getAttribute("title") === color)
+  ) {
+    let link = document.createElement("link");
+    let head = document.getElementsByTagName("head")[0];
+    link.rel = "stylesheet";
+    link.className = "alternate-style";
+    link.title = color;
+    link.type = "text/css";
+    link.setAttribute("disabled", "");
+
+    link.href = `/css/skins/${color}.css`;
+    head.appendChild(link);
+
+    let links = document.querySelectorAll(".alternate-style"),
+      totalLinks = links.length;
+
+    for (let i = 0; i < totalLinks; i++) {
+      if (color === links[i].getAttribute("title")) {
+        links[i].removeAttribute("disabled");
+      } else {
+        links[i].setAttribute("disabled", true);
+      }
+    }
+  } else {
+    for (let i = 0; i < totalLinks; i++) {
+      if (color === links[i].getAttribute("title")) {
+        links[i].removeAttribute("disabled");
+      } else {
+        links[i].setAttribute("disabled", true);
+      }
     }
   }
 }
